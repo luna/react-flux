735e71e7b2f7bb3ee5793f84fbf52845ab16bdb2
Piotr M?odawski <pmlodawski@gmail.com>
2017-01-11 15:49:27 +0100

    Use Eq to compare if state or props changed

--- jsbits/views.js
+++ jsbits/views.js
@@ -1,6 +1,6 @@
 /* jshint sub:true */
 
-function hsreact$mk_class(name, renderCb, checkState, releaseState) {
+function hsreact$mk_class(name, renderCb, checkState, releaseState, compProps, compState) {
     var cl = {
         'displayName': name,
         'componentWillReceiveProps': function() {
@@ -33,54 +33,14 @@ function hsreact$mk_class(name, renderCb, checkState, releaseState) {
         _currentCallbacks: []
     };
 
-    //Checks if the javascript representations of two haskell values are the same.
-    //This can't check equality but just checks if the javascript object has not been
-    //changed.  We have special support for tuples of size 2 and 3, where we do check if the individual components of
-    //the tuple are equal.
-    var areValuesSame = function(obj1, obj2) {
-        if (obj1 == obj2) { //use two equal signs to test if the objects are the same
-            return true;
 
-        } else {
-
-            //check tuples of size two and 3.
-            //
-            //Tuples of size 2 are stored as
-            //  { d1: first value
-            //  , d2: second value
-            //  , f: constructor function
-            //  }
-            //
-            //Tuples of size 3 are stored as
-            //  { d1: first value
-            //  , d2: { d1: second value
-            //        , d2: third value
-            //        }
-            //  , f: constructor function
-            //  }
-            var obj1_f = (obj1['f'] || {})['name'];
-            var obj2_f = (obj2['f'] || {})['name'];
-
-            if (obj1_f === "h$ghczmprimZCGHCziTupleziZLz2cUZR_con_e" && obj1_f === obj2_f) {
-                //pair
-                return obj1['d1'] == obj2['d1'] && obj1['d2'] == obj2['d2']; //use two equal signs to test if the objects are the same
-
-            } else if (obj1_f === "h$ghczmprimZCGHCziTupleziZLz2cUz2cUZR_con_e" && obj1_f === obj2_f) {
-                var obj1_d2 = obj1['d2'] || {};
-                var obj2_d2 = obj2['d2'] || {};
-                return obj1['d1'] == obj2['d1'] && obj1_d2['d1'] == obj2_d2['d1'] && obj1_d2['d2'] == obj2_d2['d2'];
-            } else {
-                return false;
-            }
-        }
-    };
     if (checkState) {
         cl['shouldComponentUpdate'] = function(newProps, newState) {
-            return !areValuesSame(this['props'].hs.root, newProps.hs.root) || !areValuesSame(this['state'].hs.root, newState.hs.root);
+            return !compProps(this['props'].hs, newProps.hs) || !compState(this['state'].hs, newState.hs);
         };
     } else {
         cl['shouldComponentUpdate'] = function(newProps, newState) {
-            return !areValuesSame(this['props'].hs.root, newProps.hs.root);
+            return !compProps(this['props'].hs, newProps.hs);
         };
     }
 
@@ -89,12 +49,12 @@ function hsreact$mk_class(name, renderCb, checkState, releaseState) {
             'intl': ReactIntl['intlShape']
         };
     }
-    
+
     return cl;
 }
 
-function hsreact$mk_ctrl_view(name, store, renderCb) {
-    var cl = hsreact$mk_class(name, renderCb, true, false);
+function hsreact$mk_ctrl_view(name, store, renderCb, compProps, compState) {
+    var cl = hsreact$mk_class(name, renderCb, true, false, compProps, compState);
     cl['getInitialState'] = function() {
         return {hs: store.sdata};
     };
@@ -110,12 +70,12 @@ function hsreact$mk_ctrl_view(name, store, renderCb) {
     return React['createClass'](cl);
 }
 
-function hsreact$mk_view(name, renderCb) {
-    return React['createClass'](hsreact$mk_class(name, renderCb, false, false));
+function hsreact$mk_view(name, renderCb, compProps) {
+    return React['createClass'](hsreact$mk_class(name, renderCb, false, false, compProps));
 }
 
-function hsreact$mk_stateful_view(name, initialState, renderCb) {
-    var cl = hsreact$mk_class(name, renderCb, true, true);
+function hsreact$mk_stateful_view(name, initialState, renderCb, compProps, compState) {
+    var cl = hsreact$mk_class(name, renderCb, true, true, compProps, compState);
     cl['getInitialState'] = function() {
         return { hs: initialState };
     };
@@ -123,8 +83,8 @@ function hsreact$mk_stateful_view(name, initialState, renderCb) {
 }
 
 function hsreact$mk_lifecycle_view(name, initialState, renderCb,
-            willMountCb, didMountCb, willRecvPropsCb, willUpdateCb, didUpdateCb, willUnmountCb) {
-    var cl = hsreact$mk_class(name, renderCb, true, true);
+            willMountCb, didMountCb, willRecvPropsCb, willUpdateCb, didUpdateCb, willUnmountCb, compProps, compState) {
+    var cl = hsreact$mk_class(name, renderCb, true, true, compProps, compState);
 
     cl['getInitialState'] = function() {
         return { hs: initialState };
